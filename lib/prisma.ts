c936c0b44a754398072b42c63f4c94f9b38b7479
sanaevvv <sanaevvv@gmail.com>
2024-08-11 22:38:51 +0900

    prisma初期設定修正

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,41 +1,19 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaD1 } from '@prisma/adapter-d1';
-import { D1Database } from '@cloudflare/workers-types';
+import { Pool } from '@neondatabase/serverless';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prismaClientSingleton = () => {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL});
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({ adapter });
+};
 
-let prisma: PrismaClient;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient({
-    adapter: new PrismaD1(process.env.DB as unknown as D1Database),
-  });
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
 
-// import { PrismaClient } from '@prisma/client';
-// import { PrismaD1 } from '@prisma/adapter-d1';
-// import { D1Database } from '@cloudflare/workers-types';
-
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// let prisma: PrismaClient;
-
-// if (!global.prisma) {
-//   global.prisma = new PrismaClient({
-//     adapter: new PrismaD1(process.env.DB as unknown as D1Database),
-//   });
-// }
-// prisma = global.prisma;
-
-// export default prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;

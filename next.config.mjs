96ad6afa796e4bf3393ea5bdc63933f1dc195139
sanaevvv <sanaevvv@gmail.com>
2024-08-14 20:20:02 +0900

    cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // ESMモジュールの処理
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    // node_modulesディレクトリ内のESMモジュールを処理
+    config.module.rules.push({
+      test: /\.m?js$/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
